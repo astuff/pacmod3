efadf0208a78360a83d95c5fa5f3cd623edbad75
Josh Whitley <jwhitley@autonomoustuff.com>
2017-12-19 16:29:32 -0600

    Initial commit from ros_pacmod.

---
+++ README.md
@@ -0,0 +1,14 @@
+# PACMod (Platform Actuation and Control MODule) Vehicle Interface #
+
+[![Build Status](https://travis-ci.org/astuff/ros_pacmod.svg?branch=master)](https://travis-ci.org/astuff/ros_pacmod)
+
+This ROS node is designed to allow the user to control a vehicle (see SUPPORTED VEHICLES below) with the PACMod drive-by-wire system. For more information about the topics, parameters, and details on the implementation, see [the wiki page](https://autonomoustuff.atlassian.net/wiki/spaces/RW/pages/17749288/PACMod+System).
+
+## Supported Vehicles ##
+
+- Polaris GEM Series (e2/e4/e6) MY 2016+
+- Polaris eLXD MY 2016+
+- Polaris Ranger X900
+- International Prostar+ 122
+- Lexus RX-450h MY 2016+
+- More coming soon...
---
+++ pacmod/include/pacmod/pacmod_common.h
@@ -0,0 +1,37 @@
+#ifndef PACMOD_COMMON_H
+#define PACMOD_COMMON_H
+
+/*
+* Unpublished Copyright (c) 2009-2017 AutonomouStuff, LLC, All Rights Reserved.
+*
+* This file is part of the PACMod ROS 1.0 driver which is released under the MIT license.
+* See file LICENSE included with this software or go to https://opensource.org/licenses/MIT for full license details.
+*/
+
+#include <memory>
+#include <mutex>
+
+#include <pacmod_core.h>
+#include <ros/ros.h>
+
+#include <pacmod_msgs/DateTimeRpt.h>
+#include <pacmod_msgs/GlobalRpt.h>
+#include <pacmod_msgs/LatLonHeadingRpt.h>
+#include <pacmod_msgs/MotorRpt1.h>
+#include <pacmod_msgs/MotorRpt2.h>
+#include <pacmod_msgs/MotorRpt3.h>
+#include <pacmod_msgs/PacmodCmd.h>
+#include <pacmod_msgs/ParkingBrakeStatusRpt.h>
+#include <pacmod_msgs/PositionWithSpeed.h>
+#include <pacmod_msgs/SteeringPIDRpt1.h>
+#include <pacmod_msgs/SteeringPIDRpt2.h>
+#include <pacmod_msgs/SteeringPIDRpt3.h>
+#include <pacmod_msgs/SteeringPIDRpt4.h>
+#include <pacmod_msgs/SystemRptFloat.h>
+#include <pacmod_msgs/SystemRptInt.h>
+#include <pacmod_msgs/VehicleSpeedRpt.h>
+#include <pacmod_msgs/VinRpt.h>
+#include <pacmod_msgs/WheelSpeedRpt.h>
+#include <pacmod_msgs/YawRateRpt.h>
+
+#endif
